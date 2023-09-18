4a2e1e980ebcfa22682a2645275bee00f3826554
larcondo <larcondo90@gmail.com>
2023-09-18 18:54:51 -0300

    add logout action

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useQuery } from '@apollo/client'
+import { useApolloClient, useQuery } from '@apollo/client'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import PhoneForm from './components/PhoneForm'
@@ -10,9 +10,16 @@ function App() {
   const [token, setToken] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const result = useQuery(ALL_PERSONS)
+  const client = useApolloClient
 
   if (result.loading) {
     return <div>loading...</div>
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
   }
 
   const notify = (message) => {
@@ -24,20 +31,21 @@ function App() {
 
   if (!token) {
     return (
-      <div>
+      <>
         <Notify errorMessage={errorMessage} />
         <h2>Login</h2>
         <LoginForm
           setToken={setToken}
           setError={notify}
-        />
-      </div>
+        />  
+      </>
     )
   }
 
   return (
     <div>
       <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
